0152b79338190c7c403c71a245e9c9ee69d0c827
Manuela Carvalho de Almeida <60411875+mcalmeida13@users.noreply.github.com>
2020-03-01 15:58:46 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix
+makeCacheMatrix <- function(x=matrix()){
+  inv <- NULL
+  set <- function(y){
+      x <<- y
+      inv <<- NULL
+  }
+  get <- function() x
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+    setinv <-function(inverse) inv <<-inverse
+    getinv <- function() inv
+  list(get=get, set=set, getinv=getinv, setinv=setinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("inverse is cached")
+    return(inv)
+  }
+  m <- x$get()
+  inv <- solve(m, ...)
+  x$setinv(inv)
+  inv
 }
+
+# Testing the Function
+
+## Define the matrix
+m <- matrix(c(0, 2, 1, 0), nrow = 2, ncol = 2, byrow = TRUE)
+m
+     [,1] [,2]
+[1,]    0    2
+[2,]    1    0
+
+## Store function inverse
+m2 <- makeCacheMatrix(m)
+
+## Solve the inverse and store in a new matrix
+m3 <- cacheSolve(m2)
+     [,1] [,2]
+[1,]  0.0    1
+[2,]  0.5    0
+
+## Testing the inverse
+m%*%m3
+     [,1] [,2]
+[1,]    1    0
+[2,]    0    1
+
+m3%*%m
+     [,1] [,2]
+[1,]    1    0
+[2,]    0    1
